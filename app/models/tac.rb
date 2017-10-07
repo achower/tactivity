5eb573215b3012b0c458e5ee0ac292a1c73d4e61
Alex Hower <alex.c.hower@gmail.com>
2017-10-07 19:34:07 -0400

    Update show view, add glyphicons, and change root path

--- app/models/tac.rb
+++ app/models/tac.rb
@@ -1,5 +1,7 @@
 class Tac < ApplicationRecord
 	belongs_to :user
-	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
+	has_attached_file :image, :styles => { :large => "600x600>", :medium => "300x300>", :thumb => "100x100>" }
+	validates :image, presence: true
 	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+	validates :description, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :tacs
   devise_for :users
-  root 'pages#home'
+  root 'tacs#index'
   get 'about', to: 'pages#about'
 end
