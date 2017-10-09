2464add4418efb1c248398229728b74dba956ade
Alex Hower <alex.c.hower@gmail.com>
2017-10-08 22:17:23 -0400

    Changed Delete Color, updated About page, changed user model so that when a user cancels their account it will delete any tacs they have added

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_many :tacs
+  has_many :tacs, dependent: :destroy
   validates :name, presence: true
 end
